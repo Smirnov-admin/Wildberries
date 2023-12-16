68dcf1e633477211439241eaf15d400efa5ec0b1
Smirnov-admin <91759142+kirya99.98@users.noreply.github.com>
2023-12-16 21:03:48 +0300

    Add animation to the site

---
+++ dist/index.dec74cf5.js
@@ -0,0 +1,1 @@
+new Swiper(".mySwiper",{slidesPerView:1,spaceBetween:30,keyboard:{enabled:!0},pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
